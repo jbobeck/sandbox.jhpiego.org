1c3a2bc2a6ebc795b84614d31470069c01a799da
cdunn <hello@cdunn.io>
2019-02-06 09:49:49 -0500

    init

--- understrap-child/header.php
+++ understrap-child/header.php
@@ -24,57 +24,57 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
-	
+
 
 </head>
 
 <body <?php body_class(); ?>>
 
-<div class="site view" id="page"> 
+<div class="site view" id="page">
+	<div class="wrapper">
+	<!-- ******************* The Navbar Area ******************* -->
+		<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
 
-<!-- ******************* The Navbar Area ******************* -->
-	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
+			<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+			<nav class="navbar navbar-expand-lg navbar-dark teal-color">
 
-		<nav class="navbar navbar-expand-lg navbar-dark teal-color">
-
-		<?php if ( 'container' == $container ) : ?>
-			<div class="container" >
-		<?php endif; ?>
-                
-                <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-          <img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/understrap-child/img/svg/jhpiego-logo.svg" style="width:70%;">
-        </a>
-                
-                
-
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<!-- The WordPress Menu goes here -->
-				<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav ml-auto',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 2,
-						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-					)
-				); ?>
 			<?php if ( 'container' == $container ) : ?>
-			</div><!-- .container -->
+				<div class="container" >
 			<?php endif; ?>
 
-		</nav><!-- .site-navigation -->
-        
-        
-    
+	                <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+	          <img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/understrap-child/img/svg/jhpiego-logo.svg" style="width:70%;">
+	        </a>
 
-	</div><!-- #wrapper-navbar end -->
-    
+
+
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<!-- The WordPress Menu goes here -->
+					<?php wp_nav_menu(
+						array(
+							'theme_location'  => 'primary',
+							'container_class' => 'collapse navbar-collapse',
+							'container_id'    => 'navbarNavDropdown',
+							'menu_class'      => 'navbar-nav ml-auto',
+							'fallback_cb'     => '',
+							'menu_id'         => 'main-menu',
+							'depth'           => 2,
+							'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+						)
+					); ?>
+				<?php if ( 'container' == $container ) : ?>
+				</div><!-- .container -->
+				<?php endif; ?>
+
+			</nav><!-- .site-navigation -->
+
+
+
+
+		</div><!-- #wrapper-navbar end -->
+  </div>
  
